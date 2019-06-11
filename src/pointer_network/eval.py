866406a5bfd79c0b250ed32bb55865251ec81789
Menion93 <andrea.salvoni93@gmail.com>
2019-06-11 17:57:48 +0200

    add rouge metrics file

---
+++ src/pointer_network/eval.py
@@ -0,0 +1,19 @@
+from rouge import Rouge
+import numpy as np
+
+r = Rouge()
+
+def mmap(fn, elem):
+    return list(map(fn, elem))
+
+def rouge_score(y, y_):
+    true_tok = [' '.join(mmap(str,sent)) for sent in y]
+    pred_tok = [' '.join(mmap(str,sent)) for sent in y_]
+    scores = r.get_scores(true_tok, pred_tok)
+
+    values = []
+    for key in scores[0].keys():
+        for sub_metric in ['f', 'p', 'r']:
+            mean_score = np.mean(mmap(lambda x: x[key][sub_metric], scores))
+            values.append(mean_score)
+    return values
